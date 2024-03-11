687fa6f2f627ce138b10885470df071351ff43ae
tracy0789 <mihhtraggtran@gmail.com>
2024-03-11 11:53:39 +0800

    Add Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,15 @@
+# Tracy
+# Calculator to add two numbers and display the result
+
+# 1. Input
+
+x1 = input("Type x1:")
+x2 = input("Type x2:")
+
+# 2. Process
+
+sum = int(x1)+ int(x2)
+
+# 3. Output
+
+print(f"Sum: {sum}")
